bc2c9995fc8256e72b521ea331d54e567244bc34
Suyash Agrawal <Suyash Agrawal>
2019-01-31 04:16:54 +0530

    Added working code for reflection and partial for refraction

--- Assignment1/src/main.cpp
+++ Assignment1/src/main.cpp
@@ -7,19 +7,56 @@
 
 using namespace std;
 
+typedef Triple<float> Vector3f;
+
 int main() {
     const int width = 1000;
     const int height = 1000;
     Image i1{width,height};
 
+    IllumParams ip1,ip2, ip3;
+    ip1.Ka = Vector3f(0.9,0.1,0.1);
+    ip1.Kd = Vector3f(0.9,0.1,0.1);
+    ip1.Ks = Vector3f(0.9,0.1,0.1);
+    ip1.Krg = Vector3f(0.9,0.1,0.1);
+    ip1.Ktg = Vector3f(0.9,0.1,0.1);
+
+    ip2.Ka = Vector3f(0,0,0);
+    ip2.Kd = Vector3f(0,0,0);
+    ip2.Ks = Vector3f(0,0,0);
+    ip2.Krg = Vector3f(0,0,0);
+    ip2.Ktg = Vector3f(1,1,1);
+
+    ip3.Ka = Vector3f(1,1,1);
+    ip3.Kd = Vector3f(0.7,0.7,0.7);
+    ip3.Ks = Vector3f(0.5,0.5,0.5);
+    ip3.Krg = Vector3f(1,1,1);
+    ip3.Ktg = Vector3f(1,1,1);
+
+
     Camera cam(Ray(Point(0),Triple<float>(0,0,-1)),1,90);
-    Sphere sp1(Point(0,0,-1),10,Color(1,0,0));
-    Sphere sp2(Point(0,0,-5),2,Color(0,1,0));
+    // Sphere sp3(Point(-0.5,-0.5,-2),1,ip1);
+    Sphere sp3(Point(-1,0,-6),1,ip1);
+    // Sphere sp1(Point(-1,0,-3),0.7,ip2,1);
+    Sphere sp1(Point(0,0,-6),1.5,ip2,1);
+    Sphere sp2(Point(1,0,-6),1,ip3);
+    // Sphere sp2(Point(0,0,-5),2,ip2);
+    // Sphere sp2(Point(0,0,-5),2,ip3);
+    // Sphere sp2(Point(0,0,-10),2,ip3);
+    // Light lg1(Point(9,9,9),Color(1,1,1));
+    // Light lg1(Point(0,0,-4),Color(1,1,1));
+    Light lg2(Point(0,0,10),Color(1,1,1));
     vector<Model*> models;
     models.push_back(&sp1);
     models.push_back(&sp2);
+    models.push_back(&sp3);
 
-    RenderEngine render_man(cam,i1,models);
+    vector<Light*> lights;
+    // lights.push_back(&lg1);
+    lights.push_back(&lg2);
+
+    RenderEngine render_man(cam,i1,models,lights,Color(0.2,0.2,0.2));
+    // RenderEngine render_man(cam,i1,models,lights,Color(0,0,0));
     render_man.render();
     render_man.writeImage("./sphere.ppm");
 
