afc3242380f778694a9d35c0b80eadfd214466dc
Aman Agrawal <aman71197@gmail.com>
2019-01-31 21:00:23 +0530

    Added code to read world from input file

--- Assignment1/include/Models.h
+++ Assignment1/include/Models.h
@@ -1,17 +1,17 @@
 #pragma once
 
+#include <Eigen/Dense>
 #include <cassert>
 #include <cmath>
 #include <iostream>
 #include <optional>
-#include <Eigen/Dense>
 #include "DS.h"
 
 class Model {
    public:
     const Material mat;
 
-    Model(const Material &mat) : mat{mat} {}
+    Model(const Material& mat) : mat{mat} {}
     virtual ~Model() = default;
 
     std::optional<float> getRefractiveIndex(Vector3f incident,
@@ -117,12 +117,13 @@ class Model {
                 // light strikes from opposite side
                 continue;
             }
-            Vector3f reflected = 2 * (cos_theta)*normal_corr -incident; 
+            Vector3f reflected = 2 * (cos_theta)*normal_corr - incident;
             // some duplicate code with getReflected function
             final_color += ((this->mat).Kd).cwiseProduct(intensity) * cos_theta;
             float cos_alpha = reflected.dot(view_corr);
             if (cos_alpha > 0) {
-                final_color += ((this->mat).Ks).cwiseProduct(intensity) * pow(cos_alpha,(this->mat).specular_coeff);
+                final_color += ((this->mat).Ks).cwiseProduct(intensity) *
+                               pow(cos_alpha, (this->mat).specular_coeff);
             }
         }
 
@@ -213,62 +214,68 @@ class Sphere : public Model {
     }
 };
 
+class Plane : public Model {
+   private:
+    const Ray _normal;
 
-class Plane: public Model {
-    private:
-        const Ray _normal;
-    public:
-        Plane(const Ray& normal, Material mat): Model{mat}, _normal{normal} {}
-        std::optional<float> getIntersectionLength(const Ray& r) const {
-            float cos_theta = r.dir.dot(_normal.dir);
-            if(fabs(cos_theta)<=EPSILON) return {}; // not intersecting case
-            float t = ((_normal.src-r.src).dot(_normal.dir))/cos_theta;
-            if(t<0) return {};
-            return t;
-        }
-        std::optional<Ray> getNormal(const Point& p) const { // returns outward normal
-            // TODO: check if point on the plane
-            return _normal;
-        }
-        friend std::ostream& operator<<(std::ostream& os, const Plane& pl) {
-            return os<<"Plane{normal="<<pl._normal<<"}";
-        }
+   public:
+    Plane(const Ray& normal, Material mat) : Model{mat}, _normal{normal} {}
+    std::optional<float> getIntersectionLength(const Ray& r) const {
+        float cos_theta = r.dir.dot(_normal.dir);
+        if (fabs(cos_theta) <= EPSILON) return {};  // not intersecting case
+        float t = ((_normal.src - r.src).dot(_normal.dir)) / cos_theta;
+        if (t < 0) return {};
+        return t;
+    }
+    std::optional<Ray> getNormal(
+        const Point& p) const {  // returns outward normal
+        // TODO: check if point on the plane
+        return _normal;
+    }
+    friend std::ostream& operator<<(std::ostream& os, const Plane& pl) {
+        return os << "Plane{normal=" << pl._normal << "}";
+    }
 };
 
-class Triangle: public Model {
-    private:
-        const Point _p1;
-        const Point _p2;
-        const Point _p3;
-        const Plane _plane;
-        const float _area;
+class Triangle : public Model {
+   private:
+    const Point _p1;
+    const Point _p2;
+    const Point _p3;
+    const Plane _plane;
+    const float _area;
 
-        static float getArea(Point a,Point b,Point c) {
-            return fabs((a-b).cross(a-c).norm()/2);
-        }
+    static float getArea(Point a, Point b, Point c) {
+        return fabs((a - b).cross(a - c).norm() / 2);
+    }
 
-    public:
-        Triangle(const Point& p1, const Point& p2, const Point& p3, const Material& mat):
-        Model{mat}, _p1{p1},_p2{p2},_p3{p3},
-        _plane{Ray(_p1,((_p1-_p2).cross(_p1-_p3))),mat},
-        _area{getArea(_p1,_p2,_p3)}
-        {}
-        std::optional<float> getIntersectionLength(const Ray& r) const {
-            auto intersection_len = _plane.getIntersectionLength(r);
-            if(!intersection_len) return {};
-            Point interp = r.src+intersection_len.value()*r.dir;
-            float a1 = getArea(_p1,_p2,interp);
-            float a2 = getArea(_p1,_p3,interp);
-            float a3 = getArea(_p2,_p3,interp);
-            if(fabs(a1+a2+a3-_area)<=EPSILON) return intersection_len;
-            return {};
-        }
-        std::optional<Ray> getNormal(const Point& p) const { // returns outward normal
-            return _plane.getNormal(p);
-        }
-        friend std::ostream& operator<<(std::ostream& os, const Triangle& tr) {
-            return os<<"Triangle{p1="<<tr._p1<<",p2="<<tr._p2<<",p3="<<tr._p3<<"}";
-        }
+   public:
+    Triangle(const Point& p1, const Point& p2, const Point& p3,
+             const Material& mat)
+        : Model{mat},
+          _p1{p1},
+          _p2{p2},
+          _p3{p3},
+          _plane{Ray(_p1, ((_p1 - _p2).cross(_p1 - _p3))), mat},
+          _area{getArea(_p1, _p2, _p3)} {}
+    std::optional<float> getIntersectionLength(const Ray& r) const {
+        auto intersection_len = _plane.getIntersectionLength(r);
+        if (!intersection_len) return {};
+        Point interp = r.src + intersection_len.value() * r.dir;
+        float a1 = getArea(_p1, _p2, interp);
+        float a2 = getArea(_p1, _p3, interp);
+        float a3 = getArea(_p2, _p3, interp);
+        if (fabs(a1 + a2 + a3 - _area) <= EPSILON) return intersection_len;
+        return {};
+    }
+    std::optional<Ray> getNormal(
+        const Point& p) const {  // returns outward normal
+        return _plane.getNormal(p);
+    }
+    friend std::ostream& operator<<(std::ostream& os, const Triangle& tr) {
+        return os << "Triangle{p1=" << tr._p1 << ",p2=" << tr._p2
+                  << ",p3=" << tr._p3 << "}";
+    }
 };
 
 class Quadric : public Model {
@@ -277,13 +284,12 @@ class Quadric : public Model {
 
    public:
     using Model::Model;
-    Quadric(const QuadricParams& qp, const Material& mp)
-        : _qp(qp), Model(mp) {
+    Quadric(const QuadricParams& qp, const Material& mp) : _qp(qp), Model(mp) {
         M << qp.A, qp.B, qp.C, qp.D, qp.B, qp.E, qp.F, qp.G, qp.C, qp.F, qp.H,
             qp.I, qp.D, qp.G, qp.I, qp.J;
     }
 
-    std::optional<float> getIntersectionLength(const Ray& r) const{
+    std::optional<float> getIntersectionLength(const Ray& r) const {
         auto Ro = augment(r.src, 1.0);
         auto Rd = augment(r.dir, 0.0);
         float Aq = Rd * M * (Rd.transpose());
@@ -292,14 +298,15 @@ class Quadric : public Model {
         float Cq = Ro * M * (Ro.transpose());
         auto inters = solve_quadratic(Aq, Bq, Cq);
         if (!inters) return {};
-        auto min_pos_dist = (inters.value().first < 0)?(inters.value().second):(inters.value().first);
-        if(min_pos_dist<0) return {};
+        auto min_pos_dist = (inters.value().first < 0) ? (inters.value().second)
+                                                       : (inters.value().first);
+        if (min_pos_dist < 0) return {};
         return min_pos_dist;
     }
 
     std::optional<Ray> getNormal(const Point& p) const {
         float val = augment(p, 1.0) * M * (augment(p, 1.0).transpose());
-        if (abs(val) > 10*EPSILON) return {};
+        if (abs(val) > 10 * EPSILON) return {};
         float nx = 2.0 * (_qp.A * p[0] + _qp.B * p[1] + _qp.C * p[2] + _qp.D);
         float ny = 2.0 * (_qp.B * p[0] + _qp.E * p[1] + _qp.F * p[2] + _qp.G);
         float nz = 2.0 * (_qp.C * p[0] + _qp.F * p[1] + _qp.H * p[2] + _qp.I);
