7d633b70c8bb5494ee6e9e435fe028d258309002
Suyash Agrawal <ozym4nd145@outlook.com>
2019-04-20 15:45:49 +0530

    Added green water gun

--- Assignment3/src/main/holi.cpp
+++ Assignment3/src/main/holi.cpp
@@ -24,7 +24,7 @@ const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 // camera
-Camera camera(glm::vec3(21.0f, 10.0f, 35.0f));
+Camera camera(glm::vec3(0.0f, 0.0f, 35.0f));
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
@@ -37,7 +37,7 @@ int main(int argc, char** argv)
 {
 
     // Necessary to position the camera at a position from where, we can view the complete scene.
-    camera.setYawPitch(-135.0f,-5.0f);
+    // camera.setYawPitch(-135.0f,-5.0f);
 
     // glfw: initialize and configure
     // ------------------------------
@@ -86,13 +86,17 @@ int main(int argc, char** argv)
 
     Shader particleShader("../resources/shaders/particle.vs", "../resources/shaders/particle.fs");
 
-    WaterGun blueGun(100000,{0.0f,0.0f,-20.0f},{0.0f,1.0f,0.0f},{0.0f,0.464f,0.742f,0.2f},20.0f,0.1,5.0f,10000);
-    WaterGun purpleGun(100000,{-5.0f,0.0f,-20.0f},{1.0f,1.0f,0.0f},{0.464f,0.0f,0.742f,0.3f},10.0f,0.2,3.0f,10000);
-    Dust redHoli(100000,{-15.0f,0.0f,-20.0f},{0.464f,0.1f,0.242f,0.3f},
-                    {{-5.0f,-10.0f,-25.0f},{-5.0f,0.0f,0.0f},{0.0f,0.0f,-10.0f}},
-                    {{0.0f,1.0f,0.0f},{5.0f,3.0f,0.0f},{-1.0,1.0f,0.0f}}
-                    ,{2.0f,10.0f,5.0f},
-                    1.0f,20.0f,5000,2.0f);
+    WaterGun greenGun(10000,{20.0f,0.0f,-20.0f},{-1.0f,1.0f,0.0f},{0.54f, 0.38f, 0.15f,0.2f},15.0f,0.1,5.0f,5000);
+    WaterGun blueGun(10000,{0.0f,0.0f,-20.0f},{0.0f,1.0f,0.0f},{0.0f,0.464f,0.742f,0.2f},20.0f,0.2,5.0f,5000);
+    WaterGun purpleGun(10000,{-20.0f,0.0f,-20.0f},{1.0f,1.0f,0.0f},{0.464f,0.0f,0.742f,0.3f},15.0f,0.1,5.0f,5000);
+    // Dust redHoli(100000,{-15.0f,0.0f,-20.0f},{0.464f,0.1f,0.242f,0.3f},
+    //                 {{-5.0f,-10.0f,-25.0f},{-5.0f,0.0f,0.0f},{0.0f,0.0f,-10.0f}},
+    //                 {{0.0f,1.0f,0.0f},{5.0f,3.0f,0.0f},{-1.0,1.0f,0.0f}}
+    //                 ,{2.0f,10.0f,5.0f},
+    //                 1.0f,20.0f,5000,2.0f);
+
+    // glm::vec3 backgroundColor{0.27,0.42,0.58};
+    glm::vec3 backgroundColor{0.1f,0.1f,0.1f};
 
     // render loop
     // -----------
@@ -113,8 +117,9 @@ int main(int argc, char** argv)
 
         // render
         // ------
-        glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
+        glClearColor(backgroundColor[0],backgroundColor[1],backgroundColor[2], 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 
         particleShader.use();
 
@@ -128,11 +133,13 @@ int main(int argc, char** argv)
 
         blueGun.Update(deltaTime,cameraPos);
         purpleGun.Update(deltaTime,cameraPos);
-        redHoli.Update(deltaTime,timePassed,cameraPos);
+        greenGun.Update(deltaTime,cameraPos);
+        // redHoli.Update(deltaTime,timePassed,cameraPos);
 
         blueGun.Draw(particleShader);
         purpleGun.Draw(particleShader);
-        redHoli.Draw(particleShader);
+        greenGun.Draw(particleShader);
+        // redHoli.Draw(particleShader);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
